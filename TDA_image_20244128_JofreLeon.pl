c641457721c203ce6a50ba9ff7fa7473d852d00f
Catalina Jofre <catalina.jofre.l@usach.cl>
2022-10-03 13:57:42 -0300

    creacion inicial de los tda

---
+++ TDA_image_20244128_JofreLeon.pl
@@ -0,0 +1,43 @@
+/*-----------------------------------------------------TDA IMAGE-------------------------------------------*/
+% Este archivo corresponde al TDA image, se encuentran todas los predicados necesarios para su creacion, 
+% tambien los predicados organizados segun estructura.
+
+/*-----------------------------------------------------REPRESENTACION--------------------------------------*/
+
+% Este TDA corresponde a la representacion de una imagen que se compone de diversas caracteristicas como el
+% alto y ancho, ademas de tener diferentes tipos de pixeles (pixbit-d, pixrgb-d pixhex-d), lo que le da la
+% posibilidad de reproducir diferentes aspectos de lo que es una imagen, usando los colores y la profundidad.
+
+% Es una lista que contiene el alto ancho y una lista con la cantidad correspondiente de pixeles
+/*-----------------------------------------------------CONSTRUCTORES----------------------------------------*/
+/*--------------------------------------FUNCION ------------------------------------------------------*/
+% Dominio:
+% Recorrido:
+% Descripcion:
+
+
+/*-----------------------------------------------------PREDICADOS DE PERTENENCIA---------------------------*/
+/*--------------------------------------FUNCION ------------------------------------------------------*/
+% Dominio:
+% Recorrido:
+% Descripcion:
+
+/*-----------------------------------------------------SELECTORES------------------------------------------*/
+/*--------------------------------------FUNCION ------------------------------------------------------*/
+% Dominio:
+% Recorrido:
+% Descripcion:
+
+/*-----------------------------------------------------MODIFICADORES---------------------------------------*/
+/*--------------------------------------FUNCION ------------------------------------------------------*/
+% Dominio:
+% Recorrido:
+% Descripcion:
+
+/*-----------------------------------------------------OTROS PREDICADOS------------------------------------*/
+/*--------------------------------------FUNCION ------------------------------------------------------*/
+% Dominio:
+% Recorrido:
+% Descripcion:
+
+
---
+++ TDA_pixbit_20244128_JofreLeon.pl
@@ -0,0 +1,41 @@
+/*-----------------------------------------------------TDA PIXBIT-------------------------------------------*/
+% Este archivo corresponde al TDA pixbit, se encuentran todas los predicados necesarios para su creacion, 
+% tambien los predicados organizados segun estructura.
+
+/*-----------------------------------------------------REPRESENTACION--------------------------------------*/
+% El TDA PIXBIT representa la formacion de pixeles que pueden tomar valores entre 1 y 0,ademas de incluir la
+% posicion de cada pixel, representado con las letras "x" e "y", tal como un plano cartesiano y la
+% profundidad de estos, ademas de ser la unidad fundamental de una imagen llamada bitmap-d. 
+% Representacion:
+% pixbit-d <- x (int) X y (int) X bit ([0|1]) X depth (int))
+/*-----------------------------------------------------CONSTRUCTORES----------------------------------------*/
+/*--------------------------------------FUNCION ------------------------------------------------------*/
+% Dominio:
+% Recorrido:
+% Descripcion:
+
+
+/*-----------------------------------------------------PREDICADOS DE PERTENENCIA---------------------------*/
+/*--------------------------------------FUNCION ------------------------------------------------------*/
+% Dominio:
+% Recorrido:
+% Descripcion:
+
+/*-----------------------------------------------------SELECTORES------------------------------------------*/
+/*--------------------------------------FUNCION ------------------------------------------------------*/
+% Dominio:
+% Recorrido:
+% Descripcion:
+
+/*-----------------------------------------------------MODIFICADORES---------------------------------------*/
+/*--------------------------------------FUNCION ------------------------------------------------------*/
+% Dominio:
+% Recorrido:
+% Descripcion:
+
+/*-----------------------------------------------------OTROS PREDICADOS------------------------------------*/
+/*--------------------------------------FUNCION ------------------------------------------------------*/
+% Dominio:
+% Recorrido:
+% Descripcion:
+

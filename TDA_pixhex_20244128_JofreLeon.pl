6d1ba334793a412b2a158cca21766972738b12cd
Catalina Jofre <catalina.jofre.l@usach.cl>
2022-10-03 16:22:43 -0300

    representacion de los tda

---
+++ TDA_pixhex_20244128_JofreLeon.pl
@@ -0,0 +1,41 @@
+/*-----------------------------------------------------TDA PIXHEX-------------------------------------------*/
+% Este archivo corresponde al TDA pixhex, se encuentran todas los predicados necesarios para su creacion, 
+% tambien los predicados organizados segun estructura.
+
+/*-----------------------------------------------------REPRESENTACION--------------------------------------*/
+% El TDA PIXHEX representa la unidad inicial de una imagen tipo HEXMAP  cuyos colores se representan con un
+%  unico valor escrito en forma hexadecimal, ademas de incluir la posicion de de cada pixel,representado
+%  con las letras "x" e "y", tal como un plano cartesiano y la profundidad de estos.
+% Representacion:
+%  pixhex-d <- x (int) X y (int) X hex(String) X depth
+/*-----------------------------------------------------CONSTRUCTORES----------------------------------------*/
+/*--------------------------------------FUNCION ------------------------------------------------------*/
+% Dominio:
+% Recorrido:
+% Descripcion:
+
+
+/*-----------------------------------------------------PREDICADOS DE PERTENENCIA---------------------------*/
+/*--------------------------------------FUNCION ------------------------------------------------------*/
+% Dominio:
+% Recorrido:
+% Descripcion:
+
+/*-----------------------------------------------------SELECTORES------------------------------------------*/
+/*--------------------------------------FUNCION ------------------------------------------------------*/
+% Dominio:
+% Recorrido:
+% Descripcion:
+
+/*-----------------------------------------------------MODIFICADORES---------------------------------------*/
+/*--------------------------------------FUNCION ------------------------------------------------------*/
+% Dominio:
+% Recorrido:
+% Descripcion:
+
+/*-----------------------------------------------------OTROS PREDICADOS------------------------------------*/
+/*--------------------------------------FUNCION ------------------------------------------------------*/
+% Dominio:
+% Recorrido:
+% Descripcion:
+
---
+++ TDA_pixrbg_20244128_JofreLeon.pl
@@ -0,0 +1,43 @@
+/*-----------------------------------------------------TDA PIXRGB-------------------------------------------*/
+% Este archivo corresponde al TDA pixrgb, se encuentran todas los predicados necesarios para su creacion, 
+% tambien los predicados organizados segun estructura.
+
+/*-----------------------------------------------------REPRESENTACION--------------------------------------*/
+% El TDA PIXRGB representa la unidad inicial de una imagen de tipo PIXMAP expresada con pixeles que pueden
+% tomar valores del espectro RGB (donde, Red=Rojo,Green=Verde,Blue=Azul) para representar su color, estos
+% valores se mueven entre el 0 y el 255, ademas de incluir la posicion de de cada pixel, representado con
+% las letras "x" e "y" y la profundidad de estos.
+% Representacion:
+% pixrgb-d <- x (int) X y (int) X red X green  X blue X depth
+
+/*-----------------------------------------------------CONSTRUCTORES----------------------------------------*/
+/*--------------------------------------FUNCION ------------------------------------------------------*/
+% Dominio:
+% Recorrido:
+% Descripcion:
+
+
+/*-----------------------------------------------------PREDICADOS DE PERTENENCIA---------------------------*/
+/*--------------------------------------FUNCION ------------------------------------------------------*/
+% Dominio:
+% Recorrido:
+% Descripcion:
+
+/*-----------------------------------------------------SELECTORES------------------------------------------*/
+/*--------------------------------------FUNCION ------------------------------------------------------*/
+% Dominio:
+% Recorrido:
+% Descripcion:
+
+/*-----------------------------------------------------MODIFICADORES---------------------------------------*/
+/*--------------------------------------FUNCION ------------------------------------------------------*/
+% Dominio:
+% Recorrido:
+% Descripcion:
+
+/*-----------------------------------------------------OTROS PREDICADOS------------------------------------*/
+/*--------------------------------------FUNCION ------------------------------------------------------*/
+% Dominio:
+% Recorrido:
+% Descripcion:
+

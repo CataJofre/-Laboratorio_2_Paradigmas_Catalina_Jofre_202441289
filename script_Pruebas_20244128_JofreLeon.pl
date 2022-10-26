53f59c836dea424929082f3d1329b1ff553c8618
Catalina Jofre <catalina.jofre.l@usach.cl>
2022-10-26 19:05:50 -0300

    adicion de predicados compress y rotate, y los predicados usados para su creacion

--- script_Pruebas_20244128_JofreLeon.pl
+++ script_Pruebas_20244128_JofreLeon.pl
@@ -3,4 +3,4 @@
 :- use_module(tda_pixhex_20244128_JofreLeon).
 :- use_module(tda_pixrgb_20244128_JofreLeon).
 
-% pixrgb( 0, 0, 10, 10, 10, 10, P1), pixrgb( 0, 1, 20, 20, 20, 20, P2), pixrgb( 1, 0, 30, 30, 30, 30, P3), pixrgb( 1, 1, 40, 40, 40, 40, P4), image( 2, 2, [P1, P2, P3, P4], I).
+%pixrgb( 0, 0, 10, 10, 10, 10, P1), pixrgb( 0, 1, 20, 20, 20, 20, P2), pixrgb( 1, 0, 30, 30, 30, 30, P3), pixrgb( 1, 1, 40, 40, 40, 40, P4), image( 2, 2, [P1, P2, P3, P4], I).
